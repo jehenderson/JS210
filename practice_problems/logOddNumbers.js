15a96c4ee8d658927b65c32cdaa2fe61065357cd
James Henderson <james@identyti.io>
2019-11-11 09:12:48 -0500

    -

--- practice_problems/logOddNumbers.js
+++
@@ -1,8 +0,0 @@
-var logOddNumbers = function(num) {
-    for (var i = 1; i <= num; i++) {
-        if (i % 2 != 0) {
-            console.log(String(i));
-        }
-    }
-}
-logOddNumbers(19);
--- small_problems/easy1/evenNumbers.js
+++
@@ -1,1 +0,0 @@
-for (var i = 2; i < 100; i+=2) console.log(i);
