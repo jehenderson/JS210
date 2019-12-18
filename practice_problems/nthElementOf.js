2009da1bc60dd9a3ccd6160a01193c8aaf297f21
James Henderson <james@identyti.io>
2019-12-18 15:10:56 -0500

    +this

---
+++ practice_problems/nthElementOf.js
@@ -0,0 +1,8 @@
+function nthElementOf(arr, index) {
+  return arr[index];
+}
+
+var digits = [4, 8 ,15, 16, 23, 42];
+console.log(nthElementOf(digits, 3));
+console.log(nthElementOf(digits, 8));
+console.log(nthElementOf(digits, -1));
